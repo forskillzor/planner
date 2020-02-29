acf22a78653e379164f808514a5092283d148615
Sergey Orlov <skillzorwispa@gmail.com>
2020-02-29 11:16:24 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <view-day></view-day>
+<!--    <h1>{{msg}}</h1>-->
+<!--    <p>{{ monday }}</p>-->
+<!--    <p>{{ tuesday }}</p>-->
+<!--    <p>{{ wednesday }}</p>-->
+<!--    <p>{{ thursday }}</p>-->
+<!--    <p>{{ friday }}</p>-->
+<!--    <p>{{ saturday }}</p>-->
+<!--    <p>{{ sunday }}</p>-->
+<!--    <p> Почему-то кирилица работает не так как ожидалось. Это приложение органайзер будет добавлен в протфолио по-->
+<!--      завершению работы</p>-->
+  </div>
+</template>
+
+<script>
+  import viewDay from './assets/views/planner/view-day';
+    export default {
+        name: 'app',
+        components: {
+            'view-day': viewDay,
+        },
+        data() {
+            return {
+                sunday: 'Воскресенье',
+                monday: 'Понедельник',
+                tuesday: 'Вторник',
+                wednesday: 'Среда',
+                thursday: 'Четверг',
+                friday: 'Пятница',
+                saturday: 'Суббота',
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/assets/views/planner/view-day.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <div class="day__header">
+      <p class="day-number">26</p>
+      <div class="date__wrapper">
+        <p class="date__month">Февраля</p>
+        <p class="date__day-of-week">Суббота</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "view-day"
+    }
+</script>
+
+<style scoped lang="scss">
+  h1{
+    text-align: center;
+  }
+  p{color: #4d4d4d;}
+  .day__header{
+    display: flex;
+    flex-direction: row;
+    padding: 20px 0 0 0;
+    border-bottom: 1px solid #000;
+  }
+  .day-number{
+    margin: 5px 20px;
+    font-size: 40px;
+  }
+  .date__wrapper {
+    p{
+      margin: 5px 10px;
+    }
+    .date__month{ }
+    .date__day-of-week{ font-weight: 400; }
+  }
+
+  .container{
+    width: 500px;
+    margin: 0 auto;
+    border: 1px solid #d2d2d2;
+    min-height: 1000px;
+  }
+
+</style>
