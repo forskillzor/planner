f7b1c37b0c4b7aad7329cf54a65cb3bee495b236
Sergey Orlov <skillzorwispa@gmail.com>
2020-02-29 12:50:40 +0700

    add view-day.vue

---
+++ src/assets/components/hour-component.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hour">
+    <div class="hour__time">{{hour}}</div>
+    <ul class="hour__timeline">
+      <li class="timeline__item">
+        <p class="minutes">:15</p>
+        <p class="content">dfd</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "hour-component",
+        props: ['hour'],
+    }
+</script>
+
+<style scoped lang="scss">
+  .hour{
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    min-height: 50px;
+    margin: 0;
+    background-color: #fff;
+    /*border-bottom: 2px solid #d2d2d2;*/
+
+    &__time{
+      font-size: 18px;
+      padding: 0 5px 0 20px;
+    }
+
+    &__timeline{
+      width: 100%;
+      list-style:none;
+      padding: 0;
+
+      .timeline__item{
+        display: block;
+        width: 100%;
+        border-bottom: 1px solid #d2d2d2;
+
+        .minutes{
+          display: inline;
+          font-size: 14px;
+        }
+        .content{
+          display: inline;
+        }
+      }
+    }
+  }
+</style>
--- src/assets/views/planner/view-day.vue
+++ src/assets/views/planner/view-day.vue
@@ -6,13 +6,31 @@
         <p class="date__month">Февраля</p>
         <p class="date__day-of-week">Суббота</p>
       </div>
+    </div><!-- ./header -->
+    <div class="day__category">
+      <p class="category--text">категория: Работа</p>
     </div>
+    <ul class="hours">
+      <li v-for="hour in hours" class="hour">
+        <hour :hour="hour"></hour>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import hourComponent from '../../components/hour-component';
     export default {
-        name: "view-day"
+        name: "view-day",
+        components: {
+            'hour': hourComponent,
+        },
+        data(){
+            return {
+                hours: [ 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,],
+            }
+        }
+
     }
 </script>
 
@@ -26,6 +44,7 @@
     flex-direction: row;
     padding: 20px 0 0 0;
     border-bottom: 1px solid #000;
+    margin-bottom: 40px;
   }
   .day-number{
     margin: 5px 20px;
@@ -37,6 +56,22 @@
     }
     .date__month{ }
     .date__day-of-week{ font-weight: 400; }
+  }
+  .day__category{
+    background-color: #dbdbdb;
+    .category--text{
+      text-align: center;
+      margin-bottom: 0;
+    }
+  }
+  .hours  {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    li.hour{
+      border-top: 1px solid #000;
+    }
   }
 
   .container{
