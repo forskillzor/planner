52cf88b23480c4c848caa89e63b27bc05d7af1fa
Sergey Orlov <skillzorwispa@gmail.com>
2020-03-01 11:16:43 +0700

    calendar hardcode days array nice render

--- src/assets/components/calendar.vue
+++ src/assets/components/calendar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="calendar">
     <h1>Calendar</h1>
-    <ul class="days-of-week">
+    <ul class="calendar__days-of-week">
       <li>пн</li>
       <li>вт</li>
       <li>ср</li>
@@ -10,17 +10,47 @@
       <li>сб</li>
       <li>вс</li>
     </ul>
+    <ul class="calendar__weeks">
+      <li v-for="week in weeks" class="weeks__item">
+        <ul class="week__days">
+          <li v-for="day in week" class="day">{{ day }}</li>
+        </ul>
+      </li>
+    </ul>
+
   </div>
 </template>
 
 <script>
     export default {
-        name: "calendar"
+        name: "calendar",
+        data(){
+            return {
+                weeks: [
+                    [2, 3, 4, 5, 6, 7, 8],
+                    [9, 10, 11, 12, 13, 14, 15],
+                    [16, 17, 18, 19, 20, 21, 22],
+                    [23, 24, 25, 26, 27, 28, 29],
+                ]
+            }
+        }
     }
 </script>
 
 <style lang="scss">
-  .days-of-week{
+  @import '../style/core/mixins';
+  .calendar__days-of-week{
+    //@include normalize;
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    list-style: none;
+  }
+  .calendar__weeks{
+    //@include normalize;
+    list-style: none;
+  }
+  .week__days{
+    @include normalize;
     display: grid;
     grid-template-columns: repeat(7, 1fr);
     list-style: none;
